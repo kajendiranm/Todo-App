922c2ac0aa1a99d4698e9af8f618ccf9fbe09ca5
Kajendiran M <64881916+kajendiranm@users.noreply.github.com>
2023-09-09 09:55:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+let input = document.getElementById('input')
+let button = document.querySelector('button')
+let result_box = document.querySelector('.lists')
+
+todos = [];
+window.onload = ()=>{
+    todos = JSON.parse(localStorage.getItem('todos'))
+    todos.forEach(todo=>addtodo(todo))
+}
+
+button.addEventListener("click", ()=>{
+    todos.push(input.value)
+    localStorage.setItem('todos',JSON.stringify(todos))
+    addtodo(input.value)
+    input.value=""
+
+    
+})
+
+function remove(todo){
+    let index = todos.indexOf(todo)
+    if(index>-1)
+        todos.splice(index,1)
+    localStorage.setItem('todos',JSON.stringify(todos))
+    
+}
+function addtodo(todo){
+    let para = document.createElement('p')
+    para.innerText = todo
+    result_box.appendChild(para)
+
+    para.addEventListener('click',()=>{
+        para.style.textDecoration='line-through'
+        para.style.color='red'
+        remove(para.innerText)
+    })
+
+    para.addEventListener('dblclick',()=>{
+        result_box.removeChild(para)
+        remove(para.innerText)
+    })
+}
